44820ce00faf45784b15fe303175884ccfe9e1b9
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-05-23 10:43:52 +0300

    feat: add create_envs file

---
+++ scripts/create_envs.sh
@@ -0,0 +1,14 @@
+export DATABASE_HOST=0.0.0.0
+export DATABASE_PORT=5432
+export DATABASE_USER=vadbeg
+export DATABASE_PASSWORD=password
+export DATABASE_NAME=database
+export API_PREFIX=/api/v_0
+export REACT_APP_ROOT_BACKEND_URI=http://0.0.0.0:9000/api/v_0/
+export SECRET_KEY=afsdd
+
+export GOOGLE_CLIENT_ID=476155838688-163di12pq91fs3tdjht124bp7qn4v3vs.apps.googleusercontent.com
+export GOOGLE_CLIENT_SECRET=TEpo0WcOs8PGTtrBr_KBNYEj
+export GOOGLE_DISCOVERY_URL=https://accounts.google.com/.well-known/openid-configuration
+
+export OAUTHLIB_INSECURE_TRANSPORT=1  # to allow http
